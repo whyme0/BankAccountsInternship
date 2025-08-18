d95bb97629868794cb957fa88d6ceb2b4bbc18e6
whyme0 <sparkjetstudiost@gmail.com>
2025-08-18 12:50:09 +0300

    task4: дополнения функции логирования

--- HangfireJobs/Jobs/OutboxDispatcherJob.cs
+++ HangfireJobs/Jobs/OutboxDispatcherJob.cs
@@ -2,11 +2,13 @@
 using Api.Data;
 using RabbitMQ.Client;
 using System.Text;
+using System.Text.Json;
+using Api.Presentation.EventMessages;
 using Microsoft.EntityFrameworkCore;
 
 namespace HangfireJobs.Jobs
 {
-    public class OutboxDispatcherJob(IConfiguration configuration, IConnection rabbitConnection, ILogger<OutboxDispatcherJob>? logger, IAppDbContext context) : IJob
+    public class OutboxDispatcherJob(IConfiguration configuration, IConnection rabbitConnection, ILogger<OutboxDispatcherJob> logger, IAppDbContext context) : IJob
     {
         public async Task Execute(CancellationToken cancellationToken)
         {
@@ -26,6 +28,7 @@ namespace HangfireJobs.Jobs
                 try
                 {
                     var body = Encoding.UTF8.GetBytes(msg.Payload);
+                    var evt = JsonSerializer.Deserialize<EventMessage<object>>(msg.Payload);
 
                     var props = new BasicProperties
                     {
@@ -42,6 +45,13 @@ namespace HangfireJobs.Jobs
                         body, cancellationToken);
 
                     msg.PublishedAt = DateTime.UtcNow;
+
+                    logger.LogInformation("Published event {0} {1} Correlation={2}, Latency={3} ms, retry={4}",
+                        evt.EventId,
+                        msg.RoutingKey,
+                        evt.Meta.CorrelationId,
+                        (DateTime.UtcNow - msg.OccurredAt).TotalMilliseconds,
+                        0);
                 }
                 catch (Exception ex)
                 {
